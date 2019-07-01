7b13ed87c83dfd0e0418f3c5dcb7681e24a8a6d0
Faviola1 <are18004@byui.edu>
2019-07-01 16:33:27 -0400

    update

--- assignments/lesson-10/js/forecast.js
+++ assignments/lesson-10/js/forecast.js
@@ -2,75 +2,82 @@
 //APIKEY: c2f1a30c08bf1465f982f10da498c31a
 // Preston id: 5604473
 // ****** FORECAST ***
-let forecastQuery = new XMLHttpRequest();
+let forecastRequest = new XMLHttpRequest();
 let apiURLforecast = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=c2f1a30c08bf1465f982f10da498c31a'
 
-forecastQuery.open('GET', apiURLforecast, true);
-forecastQuery.send();
+forecastRequest.open('GET', apiURLforecast, true);
+forecastRequest.send();
 
-forecastQuery.onload =  function ()
+forecastRequest.onload =  function ()
  {
-    let forecastData = JSON.parse(forecastQuery.responseText);
+    let forecastData = JSON.parse(forecastRequest.responseText);
     console.log(forecastData);
 
-    var time = forecastData.list;
+    var forecastlist = forecastData.list;
+    var fday= [];
+    var ficon = [];
+    var ftemp = [];
+    var fdesc = [];
 
-    var icon = [];
-    var temp = [];
-    var desc = [];
-
-    for(i = 0; i < time.length; i++){
+    for(i = 0; i < forecastlist.length; i++){
         
-        if(time[i].dt_txt.includes ('18:00:00')) {  
+        if(forecastlist[i].dt_txt.includes ('18:00:00')) {  
             
-            wicon.push(forecastData.list[i].weather[0].icon);
-            wtemp.push(forecastData.list[i].main.temp);
-            wdesc.push(forecastData.list[i].weather[0].description);
+            fday.push(forecastData.list[i].dt_txt);
+            ficon.push(forecastData.list[i].weather[0].icon);
+            ftemp.push(forecastData.list[i].main.temp);
+            fdesc.push(forecastData.list[i].weather[0].description);
         }
     }
-let img01 = icon[0];
-let temp01 = temp[0];
-let desc01 = desc[0];
+let day01= fday[0];     
+let img01 = ficon[0];
+let temp01 = ftemp[0];
+let desc01 = fdesc[0];
 
-let img02 = icon[1];
-let temp02 = temp[1];
-let desc02 = desc[1];
+let day02= fday[1];     
+let img02 = ficon[1];
+let temp02 = ftemp[1];
+let desc02 = fdesc[1];
 
-let img03 = icon[2];
-let temp03 = temp[2];
-let desc03 = desc[2];
+let day03= fday[2];     
+let img03 = ficon[2];
+let temp03 = ftemp[2];
+let desc03 = fdesc[2];
 
-let img04 = icon[3];
-let temp04 = temp[3];
-let desc04 = desc[3];
+let day04= fday[3];     
+let img04 = ficon[3];
+let temp04 = ftemp[3];
+let desc04 = fdesc[3];
 
-let img05 = icon[4];
-let temp05 = temp[4];
-let desc05 = desc[4];
+let day05= fday[4];     
+let img05 = ficon[4];
+let temp05 = ftemp[4];
+let desc05 = fdesc[4];
+
+document.getElementById("day1").innerHTML = day01;
+document.getElementById("icon1").setAttribute("src", "http://openweathermap.org/img/w/" + img01 + ".png");
+document.getElementById("icon1").setAttribute("alt", desc01);
+document.getElementById("temp1").innerHTML = temp01.toFixed(1) + "&deg;F";
+
+document.getElementById("day2").innerHTML = day02;
+document.getElementById("icon2").setAttribute("src", "http://openweathermap.org/img/w/" + img02 + ".png");
+document.getElementById("icon2").setAttribute("alt", desc02);
+document.getElementById("temp2").innerHTML = temp02.toFixed(1) + "&deg;F";
+
+document.getElementById("day3").innerHTML = day03;
+document.getElementById("icon3").setAttribute("src", "http://openweathermap.org/img/w/" + img03 + ".png");
+document.getElementById("icon3").setAttribute("alt", desc03);
+document.getElementById("temp3").innerHTML = temp03.toFixed(1) + "&deg;F";
+
+document.getElementById("day4").innerHTML = day04;
+document.getElementById("icon4").setAttribute("src", "http://openweathermap.org/img/w/" + img04 + ".png");
+document.getElementById("icon4").setAttribute("alt", desc04);
+document.getElementById("temp4").innerHTML = temp04.toFixed(1) + "&deg;F";
+
+document.getElementById("day5").innerHTML = day05;
+document.getElementById("icon5").setAttribute("src", "http://openweathermap.org/img/w/" + img05 + ".png");
+document.getElementById("icon5").setAttribute("alt", desc05);
+document.getElementById("temp5").innerHTML = temp05.toFixed(1) + "&deg;F";
 
 
-document.getElementById('day1').innerHTML = temp01.toFixed(0)+" &deg;F";
-document.getElementById('icon1').setAttribute("src", "https://openweathermap.org/img/w/" + img01 + ".png");
-
-
-document.getElementById('day2').innerHTML = temp02.toFixed(0)+" &deg;F";
-document.getElementById('icon2').setAttribute("src", "https://openweathermap.org/img/w/" + img02 + ".png");
-
-
-document.getElementById('day3').innerHTML = temp03.toFixed(0)+" &deg;F";
-document.getElementById('icon3').setAttribute("src", "https://openweathermap.org/img/w/" + img03 + ".png");
-
-
-document.getElementById('day4').innerHTML = temp04.toFixed(0)+" &deg;F";
-document.getElementById('icon4').setAttribute("src", "https://openweathermap.org/img/w/" + img04 + ".png");
-
-document.getElementById('day5').innerHTML = temp05.toFixed(0)+" &deg;F";
-document.getElementById('icon5').setAttribute("src", "https://openweathermap.org/img/w/" + img05 + ".png");
-
-
-document.getElementById('icon1').setAttribute("alt", desc01);
-document.getElementById('icon2').setAttribute("alt", desc02);
-document.getElementById('icon3').setAttribute("alt", desc03);
-document.getElementById('icon4').setAttribute("alt", desc04);
-document.getElementById('icon5').setAttribute("alt", desc05);
 }
--- assignments/lesson-10/js/json.js
+++ assignments/lesson-10/js/json.js
@@ -18,29 +18,36 @@
 
     //Creating the town information
     function show(jsonObj) {
-        var town = jsonObj['towns'];
-  
-       for (var i = 4; i<town.length; i++) {
-            var myArticle = document.createElement('article');
-            var myH2 = document.createElement('h2');
-            var myH3 = document.createElement('h3');
-            var myPara1 = document.createElement('p');
-            var myPara2 = document.createElement('p');
-            var myPara3 = document.createElement('p');
-           // var myList = document.createElement('ul');
 
-           myH2.textContent = town[i].name;
-           myH3.textContent = town[i].motto;
-           myPara1.textContent = 'Year Founded: ' + town[i].yearFounded;
-           myPara2.textContent = 'Current Population: ' + town[i].currentPopulation;
-           myPara3.textContent = 'Average Rainfall: ' + town[i].averageRainfall;
+        var town = jsonObj['towns'];
+        var c=0;
+  
+       for (var i = 0; i<town.length; i++) {
+          if(town[i].name=='Preston'||town[i].name=='Soda Springs'||town[i].name=='Fish Haven') 
+            {
+                var myArticle = document.createElement('article');
+                var myH2 = document.createElement('h2');
+                var myH3 = document.createElement('h3');
+                var myPara1 = document.createElement('p');
+                var myPara2 = document.createElement('p');
+                var myPara3 = document.createElement('p');
+               // var myList = document.createElement('ul');
+
+                myH2.textContent = town[i].name;
+                myH3.textContent = town[i].motto;
+                myPara1.textContent = 'Year Founded: ' + town[i].yearFounded;
+                myPara2.textContent = 'Current Population: ' + town[i].currentPopulation;
+                myPara3.textContent = 'Average Rainfall: ' + town[i].averageRainfall;
       
-        myArticle.appendChild(myH2);
-        myArticle.appendChild(myPara1);
-        myArticle.appendChild(myPara2);
-        myArticle.appendChild(myPara3);
-     //   myArticle.appendChild(myList);
-        section.appendChild(myArticle);
-    } 
+                myArticle.appendChild(myH2);
+                myArticle.appendChild(myPara1);
+                myArticle.appendChild(myPara2);
+                myArticle.appendChild(myPara3);
+            //   myArticle.appendChild(myList);
+                section.appendChild(myArticle);
+              document.getElementByClass('town')[c].appendChild(myArticle);
+              c++;
+        } 
+    }
 
     }
